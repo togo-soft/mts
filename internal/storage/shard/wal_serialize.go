aa3beb8112c439ef4e540345207219ed37c4cebd
Young Xu <xuthus5@gmail.com>
2026-05-08 17:02:13 +0800

    refactor(shard): 使用 length-prefixed 序列化格式替代 null 分隔符

--- internal/storage/shard/wal_serialize.go
+++ internal/storage/shard/wal_serialize.go
@@ -8,62 +8,41 @@ import (
 	"codeberg.org/micro-ts/mts/types"
 )
 
-// Point 序列化格式:
-// [8 bytes: timestamp][4 bytes: tag_len][N bytes: tags][4 bytes: field_count]
-// field: [4 bytes: key_len][N bytes: key][1 byte: type][N bytes: value]
+const pointVersion byte = 1
 
-func estimateSerializedSize(p *types.Point) int {
-	size := 8 + 4 + 4 // timestamp + tag_len + field_count
-
-	for k, v := range p.Tags {
-		size += len(k) + len(v) + 2
-	}
-
-	for k, v := range p.Fields {
-		size += 4 + len(k) + 1
-		switch v.GetValue().(type) {
-		case *types.FieldValue_FloatValue, *types.FieldValue_IntValue:
-			size += 8
-		case *types.FieldValue_StringValue:
-			size += 4 + len(v.GetValue().(*types.FieldValue_StringValue).StringValue)
-		case *types.FieldValue_BoolValue:
-			size += 1
-		}
-	}
-
-	return size
-}
-
+// serializePoint 将 Point 序列化为 length-prefixed 字节格式。
+//
+// 格式:
+//
+//	Version(1B) + Flags(1B) + Timestamp(8B) + TagCount(2B)
+//	+ [KeyLen(2B) + Key + ValLen(2B) + Value]...
+//	+ FieldCount(2B) + [KeyLen(2B) + Key + Type(1B) + Value]...
 func serializePoint(p *types.Point) ([]byte, error) {
-	buf := make([]byte, 0, estimateSerializedSize(p))
+	size := estimateSerializedSize(p)
+	buf := make([]byte, 0, size)
+
+	buf = append(buf, pointVersion, 0) // version + flags
 
 	var ts [8]byte
 	binary.BigEndian.PutUint64(ts[:], uint64(p.Timestamp))
 	buf = append(buf, ts[:]...)
 
-	tagLen := 0
-	for k, v := range p.Tags {
-		tagLen += len(k) + len(v) + 2
-	}
-	var tl [4]byte
-	binary.BigEndian.PutUint32(tl[:], uint32(tagLen))
-	buf = append(buf, tl[:]...)
+	var tc [2]byte
+	binary.BigEndian.PutUint16(tc[:], uint16(len(p.Tags)))
+	buf = append(buf, tc[:]...)
 
 	for k, v := range p.Tags {
+		buf = appendU16(buf, uint16(len(k)))
 		buf = append(buf, k...)
-		buf = append(buf, 0)
+		buf = appendU16(buf, uint16(len(v)))
 		buf = append(buf, v...)
-		buf = append(buf, 0)
 	}
 
-	var fc [4]byte
-	binary.BigEndian.PutUint32(fc[:], uint32(len(p.Fields)))
-	buf = append(buf, fc[:]...)
+	binary.BigEndian.PutUint16(tc[:], uint16(len(p.Fields)))
+	buf = append(buf, tc[:]...)
 
 	for k, v := range p.Fields {
-		var kl [4]byte
-		binary.BigEndian.PutUint32(kl[:], uint32(len(k)))
-		buf = append(buf, kl[:]...)
+		buf = appendU16(buf, uint16(len(k)))
 		buf = append(buf, k...)
 
 		switch val := v.GetValue().(type) {
@@ -79,9 +58,7 @@ func serializePoint(p *types.Point) ([]byte, error) {
 			buf = append(buf, vb[:]...)
 		case *types.FieldValue_StringValue:
 			buf = append(buf, 2)
-			var vl [4]byte
-			binary.BigEndian.PutUint32(vl[:], uint32(len(val.StringValue)))
-			buf = append(buf, vl[:]...)
+			buf = appendU16(buf, uint16(len(val.StringValue)))
 			buf = append(buf, val.StringValue...)
 		case *types.FieldValue_BoolValue:
 			buf = append(buf, 3)
@@ -96,101 +73,112 @@ func serializePoint(p *types.Point) ([]byte, error) {
 	return buf, nil
 }
 
+func appendU16(buf []byte, v uint16) []byte {
+	var b [2]byte
+	binary.BigEndian.PutUint16(b[:], v)
+	return append(buf, b[:]...)
+}
+
+// deserializePoint 从 bytes 反序列化为 Point。
 func deserializePoint(data []byte) (*types.Point, error) {
-	if len(data) < 16 {
-		return nil, fmt.Errorf("data too short: %d bytes", len(data))
+	if len(data) < 12 {
+		return nil, fmt.Errorf("point data too short: %d bytes", len(data))
 	}
 
-	pos := 0
+	version := data[0]
+	if version != pointVersion {
+		return nil, fmt.Errorf("unsupported point version: %d", version)
+	}
 
+	pos := 2
 	ts := int64(binary.BigEndian.Uint64(data[pos : pos+8]))
 	pos += 8
 
-	tagLen := int(binary.BigEndian.Uint32(data[pos : pos+4]))
-	pos += 4
+	tagCount := int(binary.BigEndian.Uint16(data[pos : pos+2]))
+	pos += 2
 
-	estimatedTags := tagLen / 20
-	if estimatedTags < 1 {
-		estimatedTags = 1
-	}
-	tags := make(map[string]string, estimatedTags)
-
-	if tagLen > 0 {
-		if pos+tagLen > len(data) {
-			return nil, fmt.Errorf("invalid tag length: %d", tagLen)
+	tags := make(map[string]string, tagCount)
+	for i := 0; i < tagCount; i++ {
+		if pos+2 > len(data) {
+			return nil, fmt.Errorf("point data too short for tag key len")
 		}
-		tagData := data[pos : pos+tagLen]
-		pos += tagLen
-
-		start := 0
-		var key string
-		for i := 0; i <= len(tagData); i++ {
-			if i == len(tagData) || tagData[i] == 0 {
-				if key == "" {
-					key = string(tagData[start:i])
-				} else {
-					tags[key] = string(tagData[start:i])
-					key = ""
-				}
-				start = i + 1
-			}
+		kLen := int(binary.BigEndian.Uint16(data[pos : pos+2]))
+		pos += 2
+		if pos+kLen > len(data) {
+			return nil, fmt.Errorf("point data too short for tag key")
 		}
+		key := string(data[pos : pos+kLen])
+		pos += kLen
+
+		if pos+2 > len(data) {
+			return nil, fmt.Errorf("point data too short for tag val len")
+		}
+		vLen := int(binary.BigEndian.Uint16(data[pos : pos+2]))
+		pos += 2
+		if pos+vLen > len(data) {
+			return nil, fmt.Errorf("point data too short for tag value")
+		}
+		value := string(data[pos : pos+vLen])
+		pos += vLen
+
+		tags[key] = value
 	}
 
-	if pos+4 > len(data) {
-		return nil, fmt.Errorf("data too short for field count")
+	if pos+2 > len(data) {
+		return nil, fmt.Errorf("point data too short for field count")
 	}
-	fieldCount := int(binary.BigEndian.Uint32(data[pos : pos+4]))
-	pos += 4
+	fieldCount := int(binary.BigEndian.Uint16(data[pos : pos+2]))
+	pos += 2
 
 	fields := make(map[string]*types.FieldValue, fieldCount)
-
 	for i := 0; i < fieldCount; i++ {
-		if pos+4 > len(data) {
-			return nil, fmt.Errorf("data too short for field key length")
+		if pos+2 > len(data) {
+			return nil, fmt.Errorf("point data too short for field key len")
 		}
-		keyLen := int(binary.BigEndian.Uint32(data[pos : pos+4]))
-		pos += 4
-
-		if pos+keyLen+1 > len(data) {
-			return nil, fmt.Errorf("data too short for field key")
+		kLen := int(binary.BigEndian.Uint16(data[pos : pos+2]))
+		pos += 2
+		if pos+kLen > len(data) {
+			return nil, fmt.Errorf("point data too short for field key")
 		}
-		key := string(data[pos : pos+keyLen])
-		pos += keyLen
+		key := string(data[pos : pos+kLen])
+		pos += kLen
 
+		if pos+1 > len(data) {
+			return nil, fmt.Errorf("point data too short for field type")
+		}
 		typ := data[pos]
 		pos++
 
 		switch typ {
 		case 0:
 			if pos+8 > len(data) {
-				return nil, fmt.Errorf("data too short for float64 value")
+				return nil, fmt.Errorf("point data too short for float64 value")
 			}
 			val := math.Float64frombits(binary.BigEndian.Uint64(data[pos : pos+8]))
 			pos += 8
 			fields[key] = types.NewFieldValue(val)
 		case 1:
 			if pos+8 > len(data) {
-				return nil, fmt.Errorf("data too short for int64 value")
+				return nil, fmt.Errorf("point data too short for int64 value")
 			}
 			val := int64(binary.BigEndian.Uint64(data[pos : pos+8]))
 			pos += 8
 			fields[key] = types.NewFieldValue(val)
 		case 2:
-			if pos+4 > len(data) {
-				return nil, fmt.Errorf("data too short for string length")
+			if pos+2 > len(data) {
+				return nil, fmt.Errorf("point data too short for string len")
 			}
-			valLen := int(binary.BigEndian.Uint32(data[pos : pos+4]))
-			pos += 4
-			if pos+valLen > len(data) {
-				return nil, fmt.Errorf("data too short for string value")
+			vLen := int(binary.BigEndian.Uint16(data[pos : pos+2]))
+			pos += 2
+			if pos+vLen > len(data) {
+				return nil, fmt.Errorf("point data too short for string value")
 			}
-			val := string(data[pos : pos+valLen])
-			pos += valLen
+			val := string(data[pos : pos+vLen])
+			pos += vLen
 			fields[key] = types.NewFieldValue(val)
 		case 3:
 			if pos+1 > len(data) {
-				return nil, fmt.Errorf("data too short for bool value")
+				return nil, fmt.Errorf("point data too short for bool value")
 			}
 			val := data[pos] == 1
 			pos++
@@ -205,4 +193,27 @@ func deserializePoint(data []byte) (*types.Point, error) {
 		Tags:      tags,
 		Fields:    fields,
 	}, nil
+}
+
+// estimateSerializedSize 估算序列化后的字节数。
+func estimateSerializedSize(p *types.Point) int {
+	size := 1 + 1 + 8 + 2 + 2 // version + flags + ts + tagCount + fieldCount
+
+	for k, v := range p.Tags {
+		size += 2 + len(k) + 2 + len(v)
+	}
+
+	for k, v := range p.Fields {
+		size += 2 + len(k) + 1
+		switch v.GetValue().(type) {
+		case *types.FieldValue_FloatValue, *types.FieldValue_IntValue:
+			size += 8
+		case *types.FieldValue_StringValue:
+			size += 2 + len(v.GetValue().(*types.FieldValue_StringValue).StringValue)
+		case *types.FieldValue_BoolValue:
+			size += 1
+		}
+	}
+
+	return size
 }
